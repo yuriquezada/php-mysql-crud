2cf7614bfa9e518f07c1de51aee7ce36f991b13f
Yuri Quezada <yuri_quezada@hotmail.com>
2019-11-04 17:16:45 -0500

    Manda a otra pantalla si el input ingresado no es un número

--- search.php
+++ search.php
@@ -5,8 +5,7 @@
 
     $id_search=$_POST['id_search'];
     echo $id_search;
-
-    if($id_search=="")
+    if(!is_numeric($id_search))
       {echo "Digita un documento por favor. (Ej: 123)";}
     else{
       $query = "SELECT * FROM student WHERE id=$id_search";
