07e9999cd3cc965e859e87a2ad495718c1ec658d
Yuri Quezada <yuri_quezada@hotmail.com>
2019-11-04 12:48:03 -0500

    Muestra resultado de búsqueda

--- search.php
+++ search.php
@@ -23,21 +23,53 @@
     $id_search=$_POST['id_search'];
     echo $id_search;
 
-    $query = "SELECT * FROM student WHERE id==$id_search";
-    $result = mysqli_query($conn, $query);
-    while ($row = mysqli_fetch_array($result)) {
-      // $first_name = $_POST['first_name'];
-      echo $row['last_name'];
-      if(!$result) {
-        die("Query Failed no se puede buscaaar.");
+    if($id_search=="")
+      {echo "Digita un documento por favor. (Ej: 123)";}
+    else{
+      $query = "SELECT * FROM student WHERE id=$id_search";
+      $result = mysqli_query($conn, $query);
+
+      while ($row = mysqli_fetch_array($result)){
+        echo 
+        "
+          <table width=\"100%\" border=\"1\">
+            <tr>
+              <td><b><center>IDo</center></b></td>
+              <td><b><center>First name</center></b></td>
+              <td><b><center>Last name</center></b></td>
+              <td><b><center>DNI</center></b></td>
+              <td><b><center>Program</center></b></td>
+              <td><b><center>Semester</center></b></td>
+              <td><b><center>Fee</center></b></td>
+
+            </tr>
+            <tr>
+              <td>".$row['id']."</td>
+              <td>".$row['first_name']."</td>
+              <td>".$row['last_name']."</td>
+              <td>".$row['dni']."</td>
+              <td>".$row['program']."</td>
+              <td>".$row['semester']."</td>
+              <td>".$row['fee']."</td>
+            </tr>
+          </table>
+        ";
       }
+    }
+    
+    //  {
+    //   // $first_name = $_POST['first_name'];
+    //   echo $row['last_name'];
+    //   if(!$result) {
+    //     die("Query Failed no se puede buscaaar.");
+    //   }
      
 
-      $_SESSION['message'] = $first_name;
-      $_SESSION['message_type'] = 'danger';
+    //   $_SESSION['message'] = $first_name;
+    //   $_SESSION['message_type'] = 'danger';
 
-      header('Location: index.php');
-    }
+    //   header('Location: index.php');
+    // }
     
   }
 
