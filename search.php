b2af73efedf477d221b6724712cfdd7af2ef2384
yuriquezada <yuri_quezada@hotmail.com>
2019-11-04 01:12:12 -0500

    Cambio de consulta incomplet

--- search.php
+++ search.php
@@ -1,30 +1,44 @@
 <?php
 
-include("db.php");
 
-$id_search=$_POST['id_search'];
-echo $id_search;
 
+
+
+// if(isset($_GET['id'])) {
+//   $id = $_GET['id'];
+//   $query = "DELETE FROM student WHERE id = $id";
+//   $result = mysqli_query($conn, $query);
+//   if(!$result) {
+//     die("Query Failed no se pede borrar.");
+//   }
+
+//   $_SESSION['message'] = 'Student Removed Successfully';
+//   $_SESSION['message_type'] = 'danger';
+//   header('Location: index.php');
+// }
 
   if(isset($_POST['search'])){
+    include("db.php");
 
-    // $id = $_GET['id'];
-    $query = "SELECT * FROM student WHERE id=$id_search";
+    $id_search=$_POST['id_search'];
+    echo $id_search;
+
+    $query = "SELECT * FROM student WHERE id==$id_search";
     $result = mysqli_query($conn, $query);
     while ($row = mysqli_fetch_array($result)) {
-      // $row = mysqli_fetch_array($result);
-      echo $row['id'];
+      // $first_name = $_POST['first_name'];
+      echo $row['last_name'];
       if(!$result) {
-        die("Query Failed no se pede borrar.");
+        die("Query Failed no se puede buscaaar.");
       }
-  
-      $_SESSION['message'] = 'Student Removed Successfully';
+     
+
+      $_SESSION['message'] = $first_name;
       $_SESSION['message_type'] = 'danger';
-      echo "Para escapar caracteres se hace \"así\".";
-  
+
       header('Location: index.php');
     }
-
+    
   }
 
 ?>
